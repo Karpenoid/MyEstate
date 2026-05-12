434918f742a0164b774559e9491cd0f4a1d02084
Karpenoid <mxkarpen@gmail.com>
2026-05-12 14:36:44 +0300

    Setup FSD architecture, ESLint, Prettier and shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,80 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier/flat";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    files: ["**/*.{ts,tsx,js,jsx,mjs}"],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      // 'react/no-unescaped-entities': 'off',
+
+      "react/no-unknown-property": ["warn", { ignore: ["class"] }],
+      "react/jsx-curly-brace-presence": ["error", { props: "never", children: "ignore" }],
+      "react/jsx-pascal-case": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+
+      "react-hooks/set-state-in-effect": "warn",
+      "react-hooks/preserve-manual-memoization": "warn",
+
+      // imports:
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // fs, path
+            "external", // react, next, zod
+            "internal", // @/lib, @/widgets
+            ["parent", "sibling", "index"], // ../ ./
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "next/**",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["react"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+    },
+  },
+  prettierConfig,
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
